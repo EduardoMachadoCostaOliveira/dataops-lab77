d52a1c8e95db5747b644afbca27b4e3be5817955
Eduardo Oliveira <62728087+EduardoMachadoCostaOliveira@users.noreply.github.com>
2022-05-22 12:22:57 -0300

    Update spark-etl-vacinas.py

--- spark-etl-vacinas.py
+++ spark-etl-vacinas.py
@@ -7,35 +7,35 @@ from pyspark.sql.functions import *
 if __name__ == "__main__": 
 
 # INICIAR SESSÃO SPARK 
-spark = SparkSession
-	.builder
-	.appName("SparkETL")
-	.getOrCreate() 
+	spark = SparkSession
+		.builder
+		.appName("SparkETL")
+		.getOrCreate() 
 
 # LER ARQUIVOS DO S3 ENVIADO COMO PARÃMETRO 
-vacinas = spark.read\ 
-			.option("inferSchema", "true")\ 
-			.option("header", "true")\ 
-			.option("sep",";")\ 
-			.csv(sys.argv[1])     
+	vacinas = spark.read\ 
+				.option("inferSchema", "true")\ 
+				.option("header", "true")\ 
+				.option("sep",";")\ 
+				.csv(sys.argv[1])     
 
 # TRANSOFRMAÇÃO DA COLUNA DE DOSE DA VACINA 
-vacinas = vacinas.withColumn( 
-	"dose", when(vacinas.vacina_descricao_dose.contains('1'), '1') 
-	.when(vacinas.vacina_descricao_dose.contains('2'), '2') 
-	.otherwise('Única') 
-	) 
+	vacinas = vacinas.withColumn( 
+		"dose", when(vacinas.vacina_descricao_dose.contains('1'), '1') 
+		.when(vacinas.vacina_descricao_dose.contains('2'), '2') 
+		.otherwise('Única') 
+		) 
 
 # REMOVER COLUNAS DESNECESSÁRIAS 
 # DEIXAR APENAS AS COLUNAS QUE ESTÃO NA LISTA "colunas" 
-colunas = [ 
-	"paciente_enumsexobiologico",  
-	"estabelecimento_municipio_nome", 
-	"estabelecimento_uf",   
-	"vacina_dataaplicacao",  
-	"dose",  
-	"vacina_nome" 
-] 
+	colunas = [ 
+		"paciente_enumsexobiologico",  
+		"estabelecimento_municipio_nome", 
+		"estabelecimento_uf",   
+		"vacina_dataaplicacao",  
+		"dose",  
+		"vacina_nome" 
+	] 
 vacinas = vacinas.select([column for column in vacinas.columns if column in colunas]) 
 
 # RENOMEAR COLUNAS 
